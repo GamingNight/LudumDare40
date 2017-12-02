86f0bae0ba38846b42ea6e74dffd632dec0469bf
JulesPomme <julian.moreira.fr@gmail.com>
2017-12-02 09:51:55 +0100

    player changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+    public float speed;
 
-	private Rigidbody2D rgbd;
+    private Rigidbody2D rgbd;
+    private Vector3 movement;
 
-	void Start () {
-		
-	}
-	
-	void Update () {
+    void Start() {
 
-		float h = Input.GetAxisRaw ("Horizontal");
-		float v = Input.GetAxisRaw ("Vertical");
+        rgbd = GetComponent<Rigidbody2D>();
+    }
 
+    void Update() {
 
-	}
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        movement.Set(h, v, 0f);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rgbd.MovePosition(transform.position + movement);
+    }
 }

ce069eeeac6df36f4a9de7118015af561e6c4f8b
vilboot <vilboot@hotmail.com>
2017-12-02 09:14:48 +0100

    first commit + gitignore

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject ExplosionPrefab;
+    public float initSpeed;
+    public Vector3 direction;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	
+		transform.Translate (direction * initSpeed * Time.deltaTime);
+	
+	}
+		
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("touch " + other.gameObject.name);
+		Destroy(gameObject);
+		GameObject ExplosionInstance = Instantiate<GameObject> (ExplosionPrefab, transform.position, Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rgbd;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+
+	}
+}

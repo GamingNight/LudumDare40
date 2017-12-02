8a5ffe152b4752d3e9ab9ea2ef23bf0923744cc4
vilboot <vilboot@hotmail.com>
2017-12-02 07:25:53 +0100

    bullet first

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float initSpeed;
+    public Vector3 direction;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	
+		transform.Translate (direction * initSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerShotBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShotBullet : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject bulletInstance = Instantiate<GameObject> (bulletPrefab, transform.position, Quaternion.identity);
+		}
+	}
+}

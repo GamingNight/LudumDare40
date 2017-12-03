c0e1c2932551f6f66f80663fb003ca78fe1bfa02
JulesPomme <julian.moreira.fr@gmail.com>
2017-12-03 18:19:24 +0100

    update win screen with killed friends

--- Assets/Scripts/LoadMain.cs
+++ Assets/Scripts/LoadMain.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class LoadMain : MonoBehaviour {
 
     public void Load() {
-
+        GameManager.killedInhabitants = 0;
         SceneManager.LoadScene("main");
     }
 }
---
+++ Assets/Scripts/UpdateWinScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateWinScreen : MonoBehaviour {
+
+    void Start() {
+        if (GameManager.killedInhabitants > 0) {
+            GetComponent<Text>().text = "but also sacrificed " + GameManager.killedInhabitants + " of your people";
+        } else {
+            GetComponent<Text>().text = "without sacrificing anyone";
+        }
+    }
+}

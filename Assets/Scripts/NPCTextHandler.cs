1afb1c298eb7136eded55422a3478ec097d368ee
JulesPomme <julian.moreira.fr@gmail.com>
2017-12-04 14:21:00 +0100

    bubble tests

---
+++ Assets/Scripts/NPCTextHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCTextHandler : MonoBehaviour {
+
+    public GameObject bubbleText;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        
+        if(collision.gameObject.tag == "Player") {
+            bubbleText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+
+        if (collision.gameObject.tag == "Player") {
+            bubbleText.SetActive(false);
+        }
+    }
+}
